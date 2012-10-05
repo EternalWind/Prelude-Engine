0e1d4a65349676de2df022ad6134da67c3c13ae6
aeronzhou <334988486@qq.com>
2012-10-05 19:53:00 +0800

    Update config

--- engine/Core/CoreConfig.h
+++ engine/Core/CoreConfig.h
@@ -1,6 +1,25 @@
 #ifndef PL_CORE_CORECONFIG
 #define PL_CORE_CORECONFIG
 
+#include <cstdint>
+
+#ifdef Prelude_Engine_EXPORTS
+#define PRELUDE_ENGINE __declspec(dllexport)
+#else
+#define PRELUDE_ENGINE __declspec(dllimport)
+#endif
+
+#define OUT
+#define IN
+#define interface class
+#define implements public
+
+typedef int8_t byte;
+typedef int16_t byte2;
+typedef int32_t byte4;
+typedef int64_t byte8;
+
+
 #define __DeclareClass(ClassType) \
 public: \
 	static const size_t mObjectSize; \
--- engine/Memory/Alloc.cpp
+++ engine/Memory/Alloc.cpp
@@ -246,6 +246,6 @@ void* __default_alloc::reallocate(void *p, size_t old_size, size_t new_size)
 	return result;
 }
 
-}
+} // end of namespace Memory
 
-}
+} // end of namespace pl
