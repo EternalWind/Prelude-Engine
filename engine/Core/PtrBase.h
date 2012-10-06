5c7dc1a3895ec849053797f1771faead25b36d30
aeronzhou <334988486@qq.com>
2012-10-07 00:03:36 +0800

    Ptr completed!!! Welcome to test it

---
+++ engine/Core/PtrBase.h
@@ -0,0 +1,101 @@
+#ifndef PL_CORE_PTR_BASE
+#define PL_CORE_PTR_BASE
+
+#include "Core/Config.h"
+
+#include "Memory/Allocator.h"
+
+namespace pl
+{
+namespace Core
+{
+/**
+  * Base class of Ptr<T>
+  * Store original pointer and maintains a reference count of the pointer
+  */
+template <class T>
+class PtrBase
+{
+	__DeclareClass(PtrBase)
+public:
+
+	/**
+	  * The construtor
+	  * @param p Original pointer
+	  */
+	PtrBase(T * p);
+
+	/**
+	  * Return reference count
+	  * @returns reference count
+	  */
+	int useCount();
+
+	/**
+	  * Return original pointer
+	  * @returns Original pointer 
+	  */
+	T * get() const;
+
+	/**
+	  * Add a reference count to original pointer
+	  */
+	void addRef();
+
+	/**
+	  * Release a reference count of the original pointer
+	  * When mRefCount is equal to 0, release the memory block
+	  * @returns If mRefCount is equal to 0
+	  */ 
+	bool release();
+
+
+
+private:
+	T * mOrigPtr;		//!< The naked pointer
+	int mRefCount;		//!< Reference count
+};
+
+template <class T>
+__DefiniteTemplateClass(PtrBase, T)
+
+template <class T>
+PtrBase<T>::PtrBase(T * p) 
+{
+	mOrigPtr = p;
+	mRefCount = 1;
+}
+
+template <class T>
+int PtrBase<T>::useCount()
+{
+	return mRefCount;
+}
+
+template <class T>
+T * PtrBase<T>::get() const 
+{
+	return mOrigPtr;
+}
+
+template <class T>
+void PtrBase<T>::addRef()
+{
+	++mRefCount;
+}
+
+template <class T>
+bool PtrBase<T>::release() 
+{
+	if (--mRefCount == 0)
+	{
+		delete mOrigPtr;
+		return true;
+	}
+	return false;
+}
+
+} // end of namespace Core
+
+} // end of namespace pl
+#endif
